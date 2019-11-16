9608943b1b50b538df0da80c44e5e3dd9ea9b900
TwilightKiddy <36814908+TwilightKiddy@users.noreply.github.com>
2019-11-16 06:44:03 +0300

    New update URL

--- JS/AnCB.js
+++ JS/AnCB.js
@@ -3,10 +3,10 @@
 // @match        https://animevost.org/*
 // @match        https://*.agorov.org/*
 // @version      1.1.1
-// @updateURL    https://raw.githubusercontent.com/oKiddYo/Scripts/master/JS/AnCB.js
-// @downloadURL  https://raw.githubusercontent.com/oKiddYo/Scripts/master/JS/AnCB.js
+// @updateURL    https://raw.githubusercontent.com/TwilightKiddy/Scripts/master/JS/AnCB.js
+// @downloadURL  https://raw.githubusercontent.com/TwilightKiddy/Scripts/master/JS/AnCB.js
 // @description  Resets nasty backgrounds on Animevost.org to default ones.
-// @author       oKiddYo
+// @author       Twilight Kiddy
 // @grant        none
 // @run-at       document-load
 // ==/UserScript==
